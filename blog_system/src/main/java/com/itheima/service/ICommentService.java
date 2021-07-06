345f1a7abce49ed8e1a328d104385968c7d6ba60
luojianaaa <2503282699@qq.com>
2021-07-06 20:33:36 +0800

    first commit

---
+++ blog_system/src/main/java/com/itheima/service/ICommentService.java
@@ -0,0 +1,13 @@
+package com.itheima.service;
+
+import com.github.pagehelper.PageInfo;
+import com.itheima.model.domain.Comment;
+public interface ICommentService {
+    // 获取文章下的评论
+    public PageInfo<Comment> getComments(Integer aid, int page, int count);
+
+    // 用户发表评论
+    public void pushComment(Comment comment);
+
+}
+
